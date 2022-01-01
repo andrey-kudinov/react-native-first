6d99333b3948a523566020bc537285ddbefe970d
Andrey Kudinov <2499931@gmail.com>
2022-01-02 00:13:42 +0400

    add navbar

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Navbar } from './src/Navbar';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>I love Nyuta!</Text>
+    <View>
+      <Navbar title='Todo App'/>
       <StatusBar style="auto" />
     </View>
   );
