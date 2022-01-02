6139c174f3135ae9f412fb9133d6954434fb1726
Andrey Kudinov <2499931@gmail.com>
2022-01-02 11:35:29 +0400

    add state, AddTodo, Todo

--- App.js
+++ App.js
@@ -1,21 +1,42 @@
+import { useState } from 'react'
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { Navbar } from './src/Navbar';
+import { AddTodo } from './src/AddTodo';
+import { Todo } from './src/Todo';
 
 export default function App() {
+  const [todos, setTodos] = useState([])
+
+  const addTodo = (title) => {
+    const newTodo = {
+      id: Date.now(), // ? should be String or Number for key 
+      title
+    }
+
+    setTodos(prev => [...prev, newTodo])
+  }
+
   return (
     <View>
       <Navbar title='Todo App'/>
-      <StatusBar style="auto" />
+
+      <View style={styles.container}>
+        <AddTodo onSubmit={addTodo}/>
+
+        <View>
+          {todos.map(todo => <Todo key={todo.id} todo={todo} />)}
+        </View>
+
+        {/* <StatusBar style="auto" /> */}
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingHorizontal: 30,
+    paddingVertical: 20
   },
 });
