12f1ad1d523fa39a31ed8aa985f5b89701de9273
Andrey Kudinov <2499931@gmail.com>
2022-01-02 23:50:33 +0400

    add handle remove

--- src/Todo.js
+++ src/Todo.js
@@ -1,10 +1,21 @@
-import { Text, View, StyleSheet } from 'react-native'
+import { Text, View, StyleSheet, TouchableOpacity } from 'react-native'
 
-export const Todo = ({ todo }) => {
+export const Todo = ({ todo, onRemove }) => {
+  const handleLongPress = () => {
+    onRemove(todo.id)
+  }
   return (
-    <View style={styles.todo}>
-      <Text>{todo.title}</Text>
-    </View>
+    <TouchableOpacity 
+      activeOpacity={0.5}
+      onPress={() => console.log('todo.id -', todo.id)}
+      // onLongPress={onRemove.bind(null, todo.id)}
+      // onLongPress={() => onRemove(todo.id)}
+      onLongPress={handleLongPress}
+    >
+      <View style={styles.todo}>
+        <Text>{todo.title}</Text>
+      </View>
+    </TouchableOpacity>
   )
 }
 
