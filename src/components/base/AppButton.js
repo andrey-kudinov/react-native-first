c479464373069529898b10ce25237a73e22beb6c
Andrey Kudinov <2499931@gmail.com>
2022-01-04 15:30:48 +0400

    add button component

---
+++ src/components/base/AppButton.js
@@ -0,0 +1,27 @@
+import { StyleSheet, View, TouchableOpacity } from 'react-native'
+import { AppTextBold } from './AppTextBold'
+import { THEME } from '../../theme'
+
+export const AppButton = ({ children, onPress, color = THEME.MAIN_COLOR }) => {
+  return (
+    <TouchableOpacity onPress={onPress} activeOpacity={0.7}>
+      <View style={{ ...styles.button, backgroundColor: color }}>
+        <AppTextBold style={styles.text}>{children}</AppTextBold>
+      </View>
+    </TouchableOpacity>
+  )
+}
+
+const styles = StyleSheet.create({
+  button: {
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 5
+  },
+  text: {
+    color: '#fff'
+  }
+})
