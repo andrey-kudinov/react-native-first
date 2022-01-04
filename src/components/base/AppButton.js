16ffb08a40b49438408dafe025921c823de4aee6
Andrey Kudinov <2499931@gmail.com>
2022-01-04 16:23:14 +0400

    add os feature

--- src/components/base/AppButton.js
+++ src/components/base/AppButton.js
@@ -1,14 +1,16 @@
-import { StyleSheet, View, TouchableOpacity } from 'react-native'
+import { StyleSheet, View, TouchableOpacity, TouchableNativeFeedback, Platform } from 'react-native'
 import { AppTextBold } from './AppTextBold'
 import { THEME } from '../../theme'
 
 export const AppButton = ({ children, onPress, color = THEME.MAIN_COLOR }) => {
+  const Wrapper = Platform.OS === 'android' ? TouchableNativeFeedback : TouchableOpacity
+
   return (
-    <TouchableOpacity onPress={onPress} activeOpacity={0.7}>
+    <Wrapper onPress={onPress} activeOpacity={0.7}>
       <View style={{ ...styles.button, backgroundColor: color }}>
         <AppTextBold style={styles.text}>{children}</AppTextBold>
       </View>
-    </TouchableOpacity>
+    </Wrapper>
   )
 }
 
