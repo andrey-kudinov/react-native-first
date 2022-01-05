5849de1c806289f299721ba43dff21bba99f3361
Andrey Kudinov <2499931@gmail.com>
2022-01-05 15:39:47 +0400

    add screen change

--- src/context/todo/TodoState.js
+++ src/context/todo/TodoState.js
@@ -1,24 +1,30 @@
-import { useReducer } from 'react'
+import { useReducer, useContext } from 'react'
 import { TodoContext } from './todoContext'
+import { ScreenContext } from '../screen/screenContext'
 import { todoReducer } from './todoReducer'
 import { ADD_TODO, REMOVE_TODO, UPDATE_TODO } from '../types'
 
 export const TodoState = ({ children }) => {
   const initialState = {
-    todos: [...Array(2)].map((n, i) => ({
-      id: i + 1,
-      title: String((i + 1) * 1000)
-    }))
-  },
-   [state, dispatch] = useReducer(todoReducer, initialState)
+      todos: [...Array(2)].map((n, i) => ({
+        id: i + 1,
+        title: String((i + 1) * 1000)
+      }))
+    },
+    { changeScreen } = useContext(ScreenContext),
+    [state, dispatch] = useReducer(todoReducer, initialState)
 
   const addTodo = title => dispatch({ type: ADD_TODO, title }),
-  removeTodo = id => dispatch({ type: REMOVE_TODO, id }),
-  updateTodo = (id, title) => dispatch({ type: UPDATE_TODO, id, title })
-
+    removeTodo = id => {
+      changeScreen(null)
+      dispatch({ type: REMOVE_TODO, id })
+    },
+    updateTodo = (id, title) => dispatch({ type: UPDATE_TODO, id, title })
 
   return (
-    <TodoContext.Provider value={{ todos: state.todos, addTodo, removeTodo, updateTodo }}>
+    <TodoContext.Provider
+      value={{ todos: state.todos, addTodo, removeTodo, updateTodo }}
+    >
       {children}
     </TodoContext.Provider>
   )
