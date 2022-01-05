86100e5ca0e26b333f5c36e0b46f2339a0594450
Andrey Kudinov <2499931@gmail.com>
2022-01-05 15:40:01 +0400

    add screen context

--- src/MainLayout.js
+++ src/MainLayout.js
@@ -5,10 +5,12 @@ import { MainScreen } from './screens/MainScreen'
 import { TodoScreen } from './screens/TodoScreen'
 import { THEME } from './theme'
 import { TodoContext } from './context/todo/todoContext'
+import { ScreenContext } from './context/screen/screenContext'
 
 export const MainLayout = () => {
-  const { todos, addTodo, removeTodo, updateTodo } = useContext(TodoContext),
-    [todoId, setTodoId] = useState(null)
+  const { todos, addTodo, removeTodo, updateTodo } = useContext(TodoContext)
+  const { todoId, changeScreen } = useContext(ScreenContext)
+  // const [todoId, setTodoId] = useState(null)
   // const [todos, setTodos] = useState([])
 
   // const addTodo = title => {
@@ -60,7 +62,8 @@ export const MainLayout = () => {
       addTodo={addTodo}
       removeTodo={removeTodo}
       // openTodo={id => setTodoId(id)}
-      openTodo={setTodoId}
+      // openTodo={setTodoId}
+      openTodo={changeScreen}
     />
   )
 
@@ -69,7 +72,8 @@ export const MainLayout = () => {
     content = (
       <TodoScreen
         onRemove={removeTodo}
-        goBack={() => setTodoId(null)}
+        // goBack={() => setTodoId(null)}
+        goBack={() => changeScreen(null)}
         todo={selectedTodo}
         onSave={updateTodo}
       />
