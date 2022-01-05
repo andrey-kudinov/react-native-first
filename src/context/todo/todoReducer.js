df81c49bd363bb654e150636be6a31ccbac7dd00
Andrey Kudinov <2499931@gmail.com>
2022-01-05 15:39:16 +0400

    refactor

--- src/context/todo/todoReducer.js
+++ src/context/todo/todoReducer.js
@@ -1,34 +1,64 @@
 import { ADD_TODO, REMOVE_TODO, UPDATE_TODO } from '../types'
 
+const handlers = {
+  [ADD_TODO]: (state, { title }) => ({
+    ...state,
+    todos: [
+      ...state.todos,
+      {
+        id: Date.now(),
+        title
+      }
+    ]
+  }),
+  [REMOVE_TODO]: (state, { id }) => ({
+    ...state,
+    todos: state.todos.filter(todo => todo.id !== id)
+  }),
+  [UPDATE_TODO]: (state, { id, title }) => ({
+    ...state,
+    todos: state.todos.map(todo => {
+      if (todo.id === id) {
+        todo.title = title
+      }
+      return todo
+    })
+  }),
+  DEFAULT: state => state
+}
+
 export const todoReducer = (state, action) => {
-  switch (action.type) {
-    case ADD_TODO:
-      return {
-        ...state,
-        todos: [
-          ...state.todos,
-          {
-            id: Date.now(),
-            title: action.title
-          }
-        ]
-      }
-    case REMOVE_TODO:
-      return {
-        ...state,
-        todos: state.todos.filter(todo => todo.id !== action.id)
-      }
-    case UPDATE_TODO:
-      return {
-        ...state,
-        todos: state.todos.map(todo => {
-          if (todo.id === action.id) {
-            todo.title = action.title
-          }
-          return todo
-        })
-      }
-    default:
-      return state
-  }
+  const handler = handlers[action.type] || handlers.DEFAULT
+  return handler(state, action)
+
+  // switch (action.type) {
+  //   case ADD_TODO:
+  //     return {
+  //       ...state,
+  //       todos: [
+  //         ...state.todos,
+  //         {
+  //           id: Date.now(),
+  //           title: action.title
+  //         }
+  //       ]
+  //     }
+  //   case REMOVE_TODO:
+  //     return {
+  //       ...state,
+  //       todos: state.todos.filter(todo => todo.id !== action.id)
+  //     }
+  //   case UPDATE_TODO:
+  //     return {
+  //       ...state,
+  //       todos: state.todos.map(todo => {
+  //         if (todo.id === action.id) {
+  //           todo.title = action.title
+  //         }
+  //         return todo
+  //       })
+  //     }
+  //   default:
+  //     return state
+  // }
 }
