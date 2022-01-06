987d0e388531e31316bb456e94233a86fcc3c146
Andrey Kudinov <2499931@gmail.com>
2022-01-06 23:22:47 +0400

    add update todo

--- src/screens/TodoScreen.js
+++ src/screens/TodoScreen.js
@@ -16,8 +16,8 @@ export const TodoScreen = ({ goBack }) => {
 
   const todo = todos.find(todo => todo.id === todoId)
 
-  const handleSave = title => {
-    updateTodo(todo.id, title)
+  const handleSave = async title => {
+    await updateTodo(todo.id, title)
     setModal(false)
   }
 
